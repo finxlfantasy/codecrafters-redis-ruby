42fc399d678a7f1019549c03e37b811ddea1e5b7
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-05-31 13:36:11 +0200

    pass 3rd stage

--- app/server.rb
+++ app/server.rb
@@ -1,19 +1,23 @@
 require "socket"
+
 class YourRedisServer
   def initialize(port)
     @port = port
   end
+
   def start
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
+
     server = TCPServer.new(@port)
     client = server.accept
-    client.gets
-    response = "+PONG\r"
-    client.puts response
-Explain
+
+    loop do
+      request = client.gets.chomp
+      client if request == "PING"
+        response = "$PONG\r\n"
+      client.puts(response)
     client.close
   end
 end
+
 YourRedisServer.new(6379).start
