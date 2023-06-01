714d82214548c18528e0e926878ee4a382e7b0b6
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-06-01 16:28:03 +0200

    add 3rd stage

--- app/server.rb
+++ app/server.rb
@@ -8,7 +8,8 @@ class YourRedisServer
   def start
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
+    
+    loop do 
     server = TCPServer.new(@port)
      client = server.accept
     client.gets
