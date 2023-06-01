8e18992e9ff370817ef390670f84d4941775f841
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-06-01 13:37:13 +0200

    pass 3rd stage

--- app/server.rb
+++ app/server.rb
@@ -8,10 +8,10 @@ class YourRedisServer
   def start
     puts("Logs from your program will appear here!")
 
-    server = TCPServer.new(@port)
-    client = server.accept
     loop do
-      response = "+PONG\r\n"
+      request = client.gets.chomp
+      client if request == "PING"
+        response = "+PONG\r\n"
       client.puts(response)
     end 
 
